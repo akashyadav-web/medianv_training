5d86ded8afae778f67471356a6b328929ec2cf8a
akash <ait.17bcs4535@gmail.com>
2026-01-06 12:47:16 +0530

    fimprovemetns done

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,15 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Task } from '../tasks/task.entity';
+import { IsString } from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
+  @IsString()
   id: string;
 
   @Column({ unique: true })
+   @IsString()
   username: string;
 
   @Column()
+   @IsString()
   password: string;
 
   @OneToMany(() => Task, (task) => task.user)
